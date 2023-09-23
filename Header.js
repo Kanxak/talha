acc1263f0a7871144e637af5c6d5097256537b6a
Kanxak <142275299+Kanxak@users.noreply.github.com>
2023-09-23 15:34:20 +1200

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // images
-import Logo from  '../assets/logo.svg';
+import Logo from  '../assets/Logo.svg';
 
 const Header = () => {
   return (
@@ -8,8 +8,8 @@ const Header = () => {
     <div className='container mx-auto'>
       <div className='flex justify-between items-center'>
         {/* logo */}
-        <a href='#'>
-          <img src={Logo} alt='' />
+        <a href='http://localhost:3000/static/media/Logo.23bf098723dbd86767e7d6084d88a61f.svg'>
+          <img src={Logo} alt='Sumtingwrong' />
         </a>
         {/* button */}
         <button className='btn btn-sm'>Work with me</button>
